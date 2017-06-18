0b25f0cc7fc47a6e59c60e9cded93f62b2e35a1c
David <david_elvir@unitec.edu>
2017-06-18 16:06:40 -0600

    s

--- ElfoSanador.h
+++ ElfoSanador.h
@@ -9,10 +9,10 @@ class ElfoSanador : public Elfo{
    virtual void Atacar(Personaje*);//enemigo
    virtual void Habilidad2(Personaje*);
    virtual void Habilidad1(Personaje*);
-   virtual void Comprar(double)=0;
+   /*virtual void Comprar(double)=0;
    .
    virtual void CortarPelo(double)=0;
-   virtual void ArmaEspecial(Personaje*,Personaje*)=0;
+   virtual void ArmaEspecial(Personaje*,Personaje*)=0;*/
 
 };
 
